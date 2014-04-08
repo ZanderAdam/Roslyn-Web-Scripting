21a5e30140784c5b638d808b5e83880b61e741a9
Aleksandar Adamovic <aadamovic@tehnik-design.com>
2014-04-08 17:06:14 -0400

    Initial Code Commit

---
+++ src/RoslynWebEngine.Browser/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+
+namespace RoslynWebEngine.Browser
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var curDir = Directory.GetCurrentDirectory();
+            WebBrowser.Navigate(new Uri(String.Format("file:///{0}/SampleWebApp/index.html", curDir)));
+        }
+    }
+}
